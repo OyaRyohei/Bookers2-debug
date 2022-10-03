beb9cf5ad9b7cb76a5e6ad7ba619b13a18742edb
EC2 Default User <ec2-user@ip-172-31-80-84.ec2.internal>
2022-10-03 23:41:00 +0000

    [Finish] 5-4

--- Bookers2-debug-ruby3-main/app/controllers/relationships_controller.rb
+++ Bookers2-debug-ruby3-main/app/controllers/relationships_controller.rb
@@ -19,4 +19,5 @@ class RelationshipsController < ApplicationController
     user = User.find(params[:user_id])
     @users = user.followers
   end
+  
 end
--- Bookers2-debug-ruby3-main/config/routes.rb
+++ Bookers2-debug-ruby3-main/config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   
   root to: "homes#top"
   get "/home/about", to: "homes#about"
-  
 
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
@@ -16,7 +15,6 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
