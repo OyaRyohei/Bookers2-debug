31fe5d6b8b6539648ef50eb5669c2d3f544f8262
EC2 Default User <ec2-user@ip-172-31-80-84.ec2.internal>
2022-10-08 07:26:31 +0000

    [Finish] 5-7a

--- Bookers2-debug-ruby3-main/app/models/book.rb
+++ Bookers2-debug-ruby3-main/app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
 
   validates :title,presence:true
@@ -9,7 +10,7 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   # 検索方法分岐
   def self.looks(search, word)
     if search == "perfect_match"
