109b30c2f0ffb60ad1d84f3b3e8cd8c9580d9bbc
EC2 Default User <ec2-user@ip-172-31-80-84.ec2.internal>
2022-10-08 07:46:20 +0000

    [Update] 5-7a

--- Bookers2-debug-ruby3-main/app/controllers/books_controller.rb
+++ Bookers2-debug-ruby3-main/app/controllers/books_controller.rb
@@ -12,14 +12,14 @@ class BooksController < ApplicationController
   end
 
   def index
+    # 今日の23時59分59秒から
     to  = Time.current.at_end_of_day
+    # １週間前まで
     from  = (to - 6.day).at_beginning_of_day
-    books = Book.includes(:favorited_users).
-      sort {|a,b| 
-        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=> 
-        a.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }
-     @books = Kaminari.paginate_array(books).page(params[:page]).per(25)
+    @books = Book.all.sort {|a,b| 
+      b.favorites.where(created_at: from...to).size <=> 
+      a.favorites.where(created_at: from...to).size
+    }
      @book = Book.new
   end
 
