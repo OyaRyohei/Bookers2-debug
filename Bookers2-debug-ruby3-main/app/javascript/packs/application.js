8bfceeaa4e4daf576033e3e0c3e852e35f73ca1a
EC2 Default User <ec2-user@ip-172-31-80-84.ec2.internal>
2022-10-11 22:15:25 +0000

    [jQuery追加] 5-7d

--- Bookers2-debug-ruby3-main/app/javascript/packs/application.js
+++ Bookers2-debug-ruby3-main/app/javascript/packs/application.js
@@ -8,13 +8,20 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "jquery";
+
+
+import jQuery from "jquery"
+import raty from 'raty-js'
+
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application" 
+import "../stylesheets/application"
 
 import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+window.$ = window.jQuery = require('jquery');
+require('packs/raty')
